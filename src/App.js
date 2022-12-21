eeb707f2cfcf0d9dd90d0deb34d9ded0876203d9
SatsukiKuroda <104330708+SatsukiKuroda@users.noreply.github.com>
2022-12-21 09:35:17 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./styles.css";
+import { useCallback, useMemo, useState } from "react";
+import { ChildArea } from "./ChildArea";
+
+export default function App() {
+  console.log("App");
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const onChangeText = (e) => setText(e.target.value);
+
+  const onClickOpen = () => setOpen(!open);
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+  // 再レンダリングで初期化されない変数
+  const temp = useMemo(() => 1 + 3, []);
+  console.log(temp);
+
+  return (
+    <div className="App">
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} onClickClose={onClickClose} />
+    </div>
+  );
+}
